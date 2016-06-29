991c3e9d7e355c5e6085f03786d086dfa8599b1c
asantos <asantos@localhost.localdomain>
2016-06-29 17:51:01 -0300

    utilizando estruturas

--- hello.c
+++ hello.c
@@ -1,8 +1,35 @@
 #include <stdio.h>
 
 
+struct Pessoa{
+	char nome[100];
+	int idade;
+
+};
+
+struct Pessoa agenda[100];
+
 int main(){
 
-	printf("Hello World!!");
+	int i;
+	int n_cadastros;
+
+	scanf("%d", &n_cadastros);
+	getchar();
+
+	for(i = 0; i < n_cadastros; i++){
+
+		scanf("%[^\n]s", agenda[i].nome);
+		getchar();
+
+		scanf("%d", &agenda[i].idade);
+		getchar();
+
+	}
+
+	for(i = 0; i < n_cadastros; i++){
+		printf("%d: %s, %d\n", i + 1, agenda[i].nome, agenda[i].idade);
+	}
+
 	return 0;
 }
